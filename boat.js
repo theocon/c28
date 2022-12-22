46d00dc26e296b84af0aedc7ccb60f07ef733852
theocon <115736847+theocon@users.noreply.github.com>
2022-12-22 14:05:12 +0000

    Add files via upload

---
+++ boat.js
@@ -0,0 +1,45 @@
+class Boat {
+  constructor(x, y, width, height, boatPos, boatAnimation) {
+  
+    this.animation = boatAnimation;
+    this.speed = 0.05;
+    this.body = Bodies.rectangle(x, y, width, height);
+    this.width = width;
+    this.height = height;
+
+    this.boatPosition = boatPos;
+    this.isBroken = false; // boat is sailing and boat and cannonBall are coliding
+
+    World.add(world, this.body);
+  }
+  animate() {
+    this.speed += 0.05;
+  }
+
+  //to show the collision between ball and boat and remove the boat
+  remove(index) {
+    this.animation = brokenBoatAnimation;
+    this.speed = 0.05;
+    this.width = 300;
+    this.height = 300;
+    this.isBroken = true; 
+    
+    setTimeout(() => {
+      Matter.World.remove(world, boats[index].body);
+      delete boats[index];
+    }, 2000);
+  }
+
+  display() {
+    var angle = this.body.angle;
+    var pos = this.body.position;
+    var index = floor(this.speed % this.animation.length);
+
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    imageMode(CENTER);
+    image(this.animation[index], 0, this.boatPosition, this.width, this.height);
+    pop();
+  }
+}
